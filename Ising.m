00ca9a050c43861005a0c48d862e666253521fc6
nihilsciens <33319318+nihilsciens@users.noreply.github.com>
2018-01-26 14:45:11 +0100

    Monte Carlo

--- Ising.m
+++ Ising.m
@@ -1,7 +1,7 @@
 %% Definitioner
 
 % Antal noder (måste vara en kvadrat).
-n = 2500;
+n = 100;
 % Coupling-parametern
 J = 1;
 
@@ -47,21 +47,48 @@ end
 % Beräkna Hamiltonianen
 for x=1:N
     for y=1:N
-        % Här sker operation för varje nod
-        if y < N
-            H = H + J*A(x,y)*A(x,y+1);
-        end
-        if y > 1
-            H = H + J*A(x,y)*A(x,y-1);
-        end
-        if x < N
-            H = H + J*A(x,y)*A(x+1,y);
-        end
-        if x > 1
-            H = H + J*A(x,y)*A(x-1,y);
-        end
+        H = H + J*DeltaE([x,y], A);
     end
 end
 
 %% Monte Carlo simulering
 
+% Antal itereringar
+I = 10000;
+% Position för grannar
+Z = [1 -1 0 0; 0 0 1 -1];
+
+% Utför I antal iterationer
+for i=1:I
+    % Nollställ energiförändring
+    dE = 0;
+    % Generera slumpmässig koordinat
+    C = unidrnd(N,2,1);
+    % Ändra på noden
+    A(C(1), C(2)) = 1 - A(C(1), C(2));
+    % Avgör om förändringen i energi är mindre än 0
+    dE = J*DeltaE(C, A);
+    for j=1:4
+        X = Z(:,j) + C;
+        if min(X) > 0 && max(X) < N+1
+            dE = dE + J*DeltaE(Z(:,j)+C, A);
+        end
+    end
+    % Inte mindre än 0
+    if dE > 0
+        % Ändra tillbaka
+        A(C(1), C(2)) = 1 - A(C(1), C(2));
+    end
+    % Mindre än 0
+    if dE < 0
+        % Förändra Hamiltonianen
+        H = H + dE;
+        % Måla om noden
+        C = C - 1;
+        if A(C(1)+1, C(2)+1) == 1
+            fill([C(1) C(1)+1 C(1)+1 C(1)],[C(2) C(2) C(2)+1 C(2)+1],'r')
+        else
+            fill([C(1) C(1)+1 C(1)+1 C(1)],[C(2) C(2) C(2)+1 C(2)+1],'b')
+        end
+    end
+end
